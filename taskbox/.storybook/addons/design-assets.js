f44f3f6ebc717a25c8f23c19db0f9f00de0ebf14
lalagias <jimkountanis@gmail.com>
2020-02-24 13:45:01 +0200

    Taskbox UI

---
+++ taskbox/.storybook/addons/design-assets.js
@@ -0,0 +1,76 @@
+import React, { Fragment } from "react";
+
+import { useParameter, useStorybookState, useAddonState } from "@storybook/api";
+import { addons, types } from "@storybook/addons";
+import { AddonPanel, ActionBar } from "@storybook/components";
+import { styled } from "@storybook/theming";
+
+const getUrl = input => {
+  return typeof input === "string" ? input : input.url;
+};
+
+const Iframe = styled.iframe({
+  width: "100%",
+  height: "100%",
+  border: "0 none"
+});
+const Img = styled.img({
+  width: "100%",
+  height: "100%",
+  border: "0 none",
+  objectFit: "contain"
+});
+
+const Asset = ({ url }) => {
+  if (!url) {
+    return null;
+  }
+  if (url.match(/\.(png|gif|jpeg|tiff|svg|anpg|webp)/)) {
+    return <Img alt="" src={url} />;
+  }
+
+  return <Iframe title={url} src={url} />;
+};
+
+export const Content = () => {
+  const results = useParameter("assets", []); // story's parameter being retrieved here
+  const [selected, setSelected] = useAddonState("my/design-assets", 0); // addon state being persisted here
+  const { storyId } = useStorybookState(); // the story«s unique identifier being retrieved from Storybook global state
+
+  if (results.length === 0) {
+    return null;
+  }
+
+  if (results.length && !results[selected]) {
+    setSelected(0);
+    return null;
+  }
+
+  const url = getUrl(results[selected]).replace("{id}", storyId);
+
+  return (
+    <Fragment>
+      <Asset url={url} />
+      {results.length > 1 ? (
+        <ActionBar
+          actionItems={results.map((i, index) => ({
+            title: typeof i === "string" ? `asset #${index + 1}` : i.name,
+            onClick: () => setSelected(index)
+          }))}
+        />
+      ) : null}
+    </Fragment>
+  );
+};
+
+addons.register("my/design-assets", () => {
+  addons.add("design-assets/panel", {
+    title: "assets",
+    type: types.PANEL,
+    render: ({ active, key }) => (
+      <AddonPanel active={active} key={key}>
+        <Content />
+      </AddonPanel>
+    )
+  });
+});
--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.js"],
   addons: [
+    "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-knobs",
     "@storybook/addon-links"
