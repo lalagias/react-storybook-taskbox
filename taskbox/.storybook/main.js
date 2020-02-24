a7686c33217584ba100b6e51620bf2e4b5285e71
lalagias <jimkountanis@gmail.com>
2020-02-24 13:21:12 +0200

    added knobs addon, create a longTitle test case

--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.js"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"]
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    "@storybook/addon-links"
+  ]
 };
--- taskbox/src/components/Task.stories.js
+++ taskbox/src/components/Task.stories.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Task from "./Task";
 
 export default {
   component: Task,
   title: "Task",
+  decorators: [withKnobs],
   // Our exports that end in "Data" are not stories.
   excludeStories: /.*Data$/
 };
@@ -17,13 +19,15 @@ export const taskData = {
   updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 export const actionsData = {
   onPinTask: action("onPinTask"),
   onArchiveTask: action("onArchiveTask")
 };
 
 export const Default = () => {
-  return <Task task={{ ...taskData }} {...actionsData} />;
+  return <Task task={object("task", { ...taskData })} {...actionsData} />;
 };
 
 export const Pinned = () => (
@@ -32,4 +36,8 @@ export const Pinned = () => (
 
 export const Archived = () => (
   <Task task={{ ...taskData, state: "TASK_ARCHIVED" }} {...actionsData} />
+);
+
+export const LongTitle = () => (
+  <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
 );
